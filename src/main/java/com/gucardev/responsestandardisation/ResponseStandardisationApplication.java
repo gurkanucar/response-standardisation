95143d5889f936a4d6f0260b86343903c0d8ee17
Gurkan UCAR <ucar.gurkan@hotmail.com>
2025-07-21 20:30:20 +0300

    feat: project create

---
+++ src/main/java/com/gucardev/responsestandardisation/ResponseStandardisationApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.responsestandardisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseStandardisationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponseStandardisationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/responsestandardisation/ResponseStandardisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.responsestandardisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseStandardisationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
